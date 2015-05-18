928abe52ddd9c23e944a845a8d4a91b927d2d1d8
dannydelott <dannydelott@gmail.com>
2015-05-17 18:44:12 -0700

    Add empty path object to gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/* **************************************************
+ * PATH object contains the filepaths to site assets.
+ *
+ * NOTES:
+ * APP and LIBS use arrays to maintain order when
+ * concatenating mulitiple files into one.
+ * **************************************************/
+
+var path = {};
