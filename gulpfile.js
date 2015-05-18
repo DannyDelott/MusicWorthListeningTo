50da5276f7720a6450b1da40559fba1563f191f4
dannydelott <dannydelott@gmail.com>
2015-05-17 21:05:00 -0700

    Cleanup/Reorganize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,60 @@
-/*
- * Dependencies
- */
+/* ***************
+ * NODE PACKAGES *
+ * ***************/
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var nodemon = require('gulp-nodemon');
 
-/*
- * Paths
- */
+/* ************
+ * FILE PATHS *
+ * ************/
+
 var path = {};
-path.INDEX_SRC = './client/index.html';
+
+/* Root */
 path.BOWER_COMPONENTS_DIR = './bower_components/';
-path.VENDOR_DIR = './client/dist/vendor/';
+
+/* Client */
+path.CLIENT_DIR = './client/';
+path.DIST_DIR = path.CLIENT_DIR + './dist/';
+path.VENDOR_DIR = path.DIST_DIR + 'vendor/';
+
+path.INDEX_SRC = path.CLIENT_SRC + 'index.html';
+
 path.REACT_DIR = path.BOWER_COMPONENTS_DIR + 'react/';
 path.REACT_SRC = [
   path.REACT_DIR + 'react.js',
   path.REACT_DIR + 'JSXTransformer.js'
 ];
-path.REACT_MIN = 'react-with-jsxtransformer.min.js';
+path.REACT_MIN_SRC = 'react-with-jsxtransformer.min.js';
+
+/* Server */
 path.SERVER_DIR = './server/';
+
 path.SERVER_SRC = path.SERVER_DIR + 'server.js';
-/*
- * Tasks
- */
+
+/* ************
+ * GULP TASKS *
+ * ************/
 
 gulp.task('default', []);
 gulp.task('build', ['vendor']);
 gulp.task('dev', ['nodemon']);
 
-/*******************************************************
- * Uglify and concat third party libraries/frameworks  *
- * *****************************************************/
+/* Uglify and concat third party libraries/frameworks */
 gulp.task('vendor', function() {
 
   // React
   gulp.src(path.REACT_SRC)
     .pipe(uglify())
-    .pipe(concat(path.REACT_MIN))
+    .pipe(concat(path.REACT_MIN_SRC))
     .pipe(gulp.dest(path.VENDOR_DIR));
 
 });
 
-/**********************************
- * Start the server with nodemon  *
- * ********************************/
+/* Start the server with nodemon */
 gulp.task('nodemon', function() {
   nodemon({
     script: path.SERVER_SRC,
