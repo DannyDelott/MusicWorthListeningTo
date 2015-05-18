8787b71dee1ec15d0154538054886cd2288ec72e
dannydelott <dannydelott@gmail.com>
2015-05-17 19:47:06 -0700

    Add gulp vendor task
    - Uglify and concat third party libraries/frameworks.
    - Add path object to hold file paths.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
-/* **************************************************
- * PATH object contains the filepaths to site assets.
- *
- * NOTES:
- * APP and LIBS use arrays to maintain order when
- * concatenating mulitiple files into one.
- * **************************************************/
+/*
+ * Dependencies
+ */
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
+/*
+ * Paths
+ */
 var path = {};
+path.BOWER_COMPONENTS_DIR = './bower_components/';
+path.VENDOR_DIR = './client/js/vendor/';
+path.REACT_DIR = path.BOWER_COMPONENTS_DIR + 'react/';
+path.REACT_SRC = [
+  path.REACT_DIR + 'react.js',
+  path.REACT_DIR + 'JSXTransformer.js'
+];
+path.REACT_MIN = 'react-with-JSXTransformer.min.js';
+
+/*
+ * Tasks
+ */
+
+/*******************************************************
+ * Uglify and concat third party libraries/frameworks  *
+ * *****************************************************/
+gulp.task('vendor', function() {
+
+  // React
+  gulp.src(path.REACT_SRC)
+    .pipe(uglify())
+    .pipe(concat(path.REACT_MIN))
+    .pipe(gulp.dest(path.VENDOR_DIR));
+
+});
