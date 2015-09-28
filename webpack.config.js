815fb2cb05718365aa948b530c8470bb0fdf33a2
dannydelott <dannydelott@gmail.com>
2015-09-28 11:10:57 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ['./client/main.js']
+  },
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+
+    ]
+  }
+
+};
